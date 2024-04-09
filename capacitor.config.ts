f8ead4b8322ed62fd613f7416d501e176f00c981
Renata Klepek <renata@grammario.de>
2024-04-09 23:30:53 +0200

    Building Native Apps with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionicangularbooking',
+  appName: 'ionic-angular-booking',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
