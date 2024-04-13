935bc9d2212fcc2aec79e06dc80116e66a41c234
Renata Klepek <renata@grammario.de>
2024-04-13 21:50:24 +0200

    Preparing Data & Services for the Project

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      '../assets/img/newyork.jpg',
+      139.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris!',
+      '../assets/img/paris.jpg',
+      149.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      '../assets/img/sanfrancisco.jpg',
+      99.99
+    )
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+}
