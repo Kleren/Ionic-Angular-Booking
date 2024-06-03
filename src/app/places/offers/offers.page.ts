7bfd6e0023479d04e4e7240cddb645fc60c6d6a1
Renata Klepek <renata@grammario.de>
2024-06-03 11:15:19 +0200

    Swipeable: List Items, Bookings

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+
 
 @Component({
   selector: 'app-offers',
@@ -10,11 +14,19 @@ import { Place } from '../place.model';
 export class OffersPage implements OnInit {
   //loadedPlaces: Place[] = [];
   offers: Place[];
-  constructor(private placesService: PlacesService) { }
+
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     //this.loadedPlaces = this.placesService.places;
     this.offers = this.placesService.places;
   }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
+  }
+
 
 }
